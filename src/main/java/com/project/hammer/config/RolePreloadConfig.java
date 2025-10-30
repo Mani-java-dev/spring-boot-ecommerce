43c088fadbc75de3a17f3344026bcbaed59a3f8b
Manikandan Peramaiyan <manikandan.p@datayaan.com>
2025-10-30 07:27:40 +0530

    endpoint changes

--- src/main/java/com/project/hammer/config/RolePreloadConfig.java
+++ src/main/java/com/project/hammer/config/RolePreloadConfig.java
@@ -1,40 +1,40 @@
-package com.project.hammer.config;
-
-import com.project.hammer.entity.Role;
-import com.project.hammer.repository.RoleRepo;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
+//package com.project.hammer.config;
+//
+//import com.project.hammer.entity.Role;
+//import com.project.hammer.repository.RoleRepo;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-@Slf4j
-@Component
-public class RolePreloadConfig implements ApplicationRunner {
-
-    @Autowired
-    private RoleRepo roleRepo;
-
-    //for testing purpose only :)
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        List<Role> roles= roleRepo.getAllRoles();
-        if(Objects.isNull(roles)){
-                Role roleOne=new Role();
-                roleOne.setRole("user");
-                roles.add(roleOne);
-                Role roleTwo=new Role();
-                roleTwo.setRole("admin");
-                roles.add(roleTwo);
-                roleRepo.saveAll(roles);
-                log.info("All roles saved successfully");
-        }
-
-        roleRepo.saveAll(roles);
-        log.info("Roles are already preloaded !");
-    }
-}
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Objects;
+//
+//@Slf4j
+//@Component
+//public class RolePreloadConfig implements ApplicationRunner {
+//
+//    @Autowired
+//    private RoleRepo roleRepo;
+//
+//    //for testing purpose only :)
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        List<Role> roles= roleRepo.getAllRoles();
+//        if(Objects.isNull(roles)){
+//                Role roleOne=new Role();
+//                roleOne.setRole("user");
+//                roles.add(roleOne);
+//                Role roleTwo=new Role();
+//                roleTwo.setRole("admin");
+//                roles.add(roleTwo);
+//                roleRepo.saveAll(roles);
+//                log.info("All roles saved successfully");
+//        }
+//
+////        roleRepo.saveAll(roles);
+//        log.info("Roles are already preloaded !");
+//    }
+//}
